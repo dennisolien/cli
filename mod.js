272c82eaf18b1db46ab6e4a93e9c7f8e11cccc46
dennis lien <dennis.lien.o@gmail.com>
2020-05-17 14:33:29 +0200

    Add global alias creation

--- mod.js
+++ mod.js
@@ -1,21 +1,67 @@
-const { args } = Deno;
-import { parse } from 'https://deno.land/std/flags/mod.ts';
+const { args , cwd, env} = Deno;
+import { parse } from 'https://deno.land/std@0.51.0/flags/mod.ts';
+import { ensureDir, ensureFile, readFileStr, writeFileStr, walk } from "https://deno.land/std@0.51.0/fs/mod.ts";
 
 const inputArgs = parse(args);
 
+const bashProfilePath = `${env.get('HOME')}/.bash_profile`;
+const aliasesPath = (folder) => `${env.get('HOME')}/.bsAlias/${folder}`;
+
+async function buildBashIndex(dir) {
+  const aliases = [];
+  for await (const entry of walk(dir)) {
+    if (entry && entry.isFile && entry.name !== 'index.sh') {
+      const alias = await readFileStr(`${dir}/${entry.name}`, { encoding: 'utf8' });
+      aliases.push(alias);
+    }
+  }
+  return writeFileStr(`${dir}/index.sh`, aliases.join('\n'));
+}
+
+async function updateBashProfile(ref) {
+  const org = await readFileStr(bashProfilePath, { encoding: 'utf8' });
+  if (org.includes(ref)) {
+    return true;
+  }
+  return writeFileStr(bashProfilePath, `${org}\n${ref}`);
+}
+
+
+
+
 const commands = {
   create: {
-    alias: (flags) => {
+    alias: async (flags) => {
       const isGlobal = (flags.g || flags.global);
       const originalCommand = flags.org;
       const alias = flags.alias;
+      if (!alias || !originalCommand) {
+        return null;
+      }
+      const path = cwd(); // TEMP use user root .bs/
+      const aliasGlobal = aliasesPath('global');
+      const aliasProject = aliasesPath('project');
+      await ensureDir(aliasGlobal);
+      await ensureDir(aliasProject);
+
+      if (isGlobal) {
+        await writeFileStr(`${aliasGlobal}/${alias}.sh`, `alias ${alias}="${originalCommand}"`);
+        await buildBashIndex(aliasGlobal);
+        await updateBashProfile(`source ${aliasGlobal}/index.sh`);
+        // TODO: need to update the terminal bash_profile source.
+        return true
+      }
+
+      // TODO: get the git remote origin `/alias/project/${origin}/${alias}.sh
+      return writeFileStr(`${aliasProject}/${alias}.sh`, `alias ${alias}="${originalCommand}"`);
+
       /**
        * TODO:
-       * 1. create global alias folder
-       * 2. create projects alias folder
+       * 1. X create global alias folder
+       * 2. X create projects alias folder
        * 2.2 have sub folder, named after the projects git remote origin
-       * 3. alias folders new file for each alias.
-       * 4. export to bash_profile
+       * 3. X alias folders new file for each alias.
+       * 4. X export to bash_profile
        * 5. prompt user to create git repo to store aliases.
        */
     }
